675a42e7cd40aebb132c17e03e6aea7b6a5638cb
NCM <nmitchell34@gatech.edu>
2020-06-18 10:49:53 -0400

    got AJAX queries working

---
+++ script.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    var activity
+
+
+
+    var boredURL = "https://www.boredapi.com/api/activity/"
+    
+    $.ajax({
+        url: boredURL,
+        method: "GET",
+      }).then(function (response) {
+          console.log(response)
+          $("<button>").on("click", function())
+          //["education", "recreational", "social", "diy", "charity", "cooking", "relaxation", "music", "busywork"
+
+
+
+      })
+    
+    
+    
+    })
+    var ytURL="https://www.googleapis.com/youtube/v3/search?part=snippet&q=Dog&key=AIzaSyBTBC_1X8CFQyIOAFv_AyrCOQPyejInNtI"
+    $.ajax({
+        url: ytURL,
+        method: "GET",
+    }).then(function (response){
+        console.log(response)
+    })
